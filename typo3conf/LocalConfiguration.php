f6daac7c5e26c5c3b27d1769d15f97f6e70480e8
Christian Pschorr <pschorr.christian@gmail.com>
2015-09-14 07:37:24 +0200

    Qinx TYPO3 init

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,63 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => FALSE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '$P$CZazq1i8TjCKjSXf6slhBA5ZuF5Fz0/',
+		'loginSecurityLevel' => 'rsa',
+		'versionNumberInFilename' => '0',
+	),
+	'DB' => array(
+		'database' => 'gutschein_company',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => '127.0.0.1',
+		'password' => '',
+		'port' => 3306,
+		'username' => 'root',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'extension_builder' => 'a:3:{s:15:"enableRoundtrip";s:0:"";s:15:"backupExtension";s:1:"1";s:9:"backupDir";s:35:"uploads/tx_extensionbuilder/backups";}',
+			'qxgo' => 'a:0:{}',
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'debug' => FALSE,
+		'loginSecurityLevel' => 'rsa',
+	),
+	'GFX' => array(
+		'jpg_quality' => '80',
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => 0,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => 'eff35eac083ef3e94dbab51ab5282b25eee879c5ac26a1b2233ccfd836f4bbc5c7e569ab1f0dd759f4da95f59f0190e3',
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'TYPO3',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+	),
+);
+?>
