9caf9fe81932c7f82cc57dad2454fdb41be32ffb
Christian Pschorr <pschorr.christian@gmail.com>
2015-11-05 23:40:12 +0100

    add javascript for survey extension

--- fileadmin/Resources/Private/Js/qinx.application.js
+++ fileadmin/Resources/Private/Js/qinx.application.js
@@ -1,3 +1,12 @@
 $(function() {
-	console.log('Hello Qinx');
+	// -------------------------------------------------------------------------------------------------------------------
+	// QxSurvey
+	var qxSurveyMail 		= $('input[type=text]', '.qx-survey--mail');
+	var qxSurveyChoice	= $('.qx-survey--choice');
+
+	qxSurveyChoice.on('click', function(e) {
+		if(qxSurveyMail.val().length !== 0) {
+			qxSurveyMail.closest('form').trigger('submit');
+		}
+	});
 });
