558307627d49fdeed262ab4e3ad0bc6939e34877
Christian Pschorr <pschorr.christian@gmail.com>
2015-12-13 22:03:37 +0100

    Add new field name

--- fileadmin/Resources/Private/Js/qinx.application.js
+++ fileadmin/Resources/Private/Js/qinx.application.js
@@ -1,7 +1,7 @@
 $(function() {
 	// -------------------------------------------------------------------------------------------------------------------
 	// QxSurvey
-	var qxSurveyMail 		= $('input[type=text]', '.qx-survey--mail');
+	var qxSurveyMail 		= $('input[type=text]', '.qx-survey--name');
 	var qxSurveyChoice	= $('.qx-survey--choice');
 
 	qxSurveyChoice.on('click', function(e) {
@@ -10,39 +10,39 @@ $(function() {
 		}
 	});
 
-	// -------------------------------------------------------------------------------------------------------------------
-	// QxGallery
-	$('.qx-gallery--category-images').slick({
-		dots:          	true,
-		infinite:      	false,
-		speed:         	300,
-		slidesToShow:  	1,
-		variableWidth: 	true,
-		prevArrow:			'<div class="slick-prev">Previous</div>',
-		nextArrow: 			'<div class="slick-next">Next</div>'
-	});
-
-	$('.qx-gallery--lightbox-link').colorbox({
-		maxWidth: '90%',
-		maxHeight: '90%',
-		current: null,
-		title: function() {
-			var image	= $(this);
-			var title = '';
-
-			if(image.attr('title') !== '') {
-				title += '<div class=qx-gallery--lightbox-title-name">' + image.attr('title') + '</div>';
-			}
-
-			if(image.data('dimension') !== undefined) {
-				title += '<div class=qx-gallery--lightbox-title-dimension">' + image.data('dimension') + '</div>';
-			}
-
-			if(image.data('price') !== undefined) {
-				title += '<div class=qx-gallery--lightbox-title-price">' + image.data('price') + '</div>';
-			}
-
-			return title;
-		}
-	});
+	//// -------------------------------------------------------------------------------------------------------------------
+	//// QxGallery
+	//$('.qx-gallery--category-images').slick({
+	//	dots:          	true,
+	//	infinite:      	false,
+	//	speed:         	300,
+	//	slidesToShow:  	1,
+	//	variableWidth: 	true,
+	//	prevArrow:			'<div class="slick-prev">Previous</div>',
+	//	nextArrow: 			'<div class="slick-next">Next</div>'
+	//});
+	//
+	//$('.qx-gallery--lightbox-link').colorbox({
+	//	maxWidth: '90%',
+	//	maxHeight: '90%',
+	//	current: null,
+	//	title: function() {
+	//		var image	= $(this);
+	//		var title = '';
+	//
+	//		if(image.attr('title') !== '') {
+	//			title += '<div class=qx-gallery--lightbox-title-name">' + image.attr('title') + '</div>';
+	//		}
+	//
+	//		if(image.data('dimension') !== undefined) {
+	//			title += '<div class=qx-gallery--lightbox-title-dimension">' + image.data('dimension') + '</div>';
+	//		}
+	//
+	//		if(image.data('price') !== undefined) {
+	//			title += '<div class=qx-gallery--lightbox-title-price">' + image.data('price') + '</div>';
+	//		}
+	//
+	//		return title;
+	//	}
+	//});
 });
